4a316d1cfe64a19cdd1d54e4f06e95152f66fad9
Juan Pablo Sarobe <jp.sarobe@gmail.com>
2023-10-09 00:38:58 -0300

    mejoras en el modal

--- src/components/pages/itemDetailContainer/ItemDetail.jsx
+++ src/components/pages/itemDetailContainer/ItemDetail.jsx
@@ -6,69 +6,74 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import SendIcon from "@mui/icons-material/Send";
-import { Container, maxHeight } from "@mui/system";
+import { Container, sizing } from "@mui/system";
 import { Button } from "@mui/material";
 import AddItemsContainer from "../../common/addItems/AddItemsContainer";
 
 const ItemDetail = ({ item }) => {
   return (
-    <>
-      <Container maxWidth="lg" sx={{ mt: { xs: 10, md: 20 } }}>
-        <Card
+    <Container
+      id="test"
+      maxWidth="75vw"
+      width="75%"
+      sx={{ mt: { xs: 10, md: 20 } }}
+    >
+      <Card
+        sx={{
+          display: "flex",
+          flexDirection: { xs: "column", md: "row" },
+          borderRadius: 8,
+          pt: 2,
+        }}
+      >
+        <CardMedia
+          component="img"
+          sx={{
+            width: { xs: 1, md: 1 / 2 },
+            maxHeight: { xs: 280, md: 525 },
+          }}
+          image={item.img}
+          alt={item.title}
+        />
+        <Box
           sx={{
             display: "flex",
-            flexDirection: { xs: "column", md: "row" },
-            borderRadius: 8,
+            flexDirection: "column",
+            width: { xs: 1, md: 1 / 2 },
           }}
         >
-          <CardMedia
-            component="img"
-            sx={{
-              width: { xs: 1, md: 1 / 2 },
-              maxHeight: { xs: 280, md: 525 },
-            }}
-            image={item.img}
-            alt={item.title}
-          />
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              width: { xs: 1, md: 1 / 2 },
-            }}
-          >
-            <CardContent sx={{ flex: "1 0 auto" }}>
-              <Typography component="div" variant="h3">
-                {item.title}
+          <CardContent sx={{ flex: "1 0 auto" }}>
+            <Typography component="div" variant="h3">
+              {item.title}
+            </Typography>
+            <Typography variant="h5" color="text.secondary" component="div">
+              {item.description}
+            </Typography>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: { xs: "column", sm: "row" },
+                justifyContent: "space-between",
+                alignItems: "center",
+                m: 5,
+              }}
+            >
+              <Typography component="div" variant="h4">
+                Cantidad
               </Typography>
-              <Typography variant="h5" color="text.secondary" component="div">
-                {item.description}
-              </Typography>
-              <Box
-                sx={{
-                  display: "flex",
-                  justifyContent: "space-between",
-                  alignItems: "center",
-                  m: 5,
-                }}
-              >
-                <Typography component="div" variant="h4">
-                  Cantidad
-                </Typography>
-                <Box>
-                  <AddItemsContainer item={item} />
-                </Box>
+              <Box>
+                <AddItemsContainer item={item} />
               </Box>
-            </CardContent>
-            <Box sx={{ display: "flex", alignItems: "center", pl: 1, pb: 1 }}>
-              <Button variant="contained" endIcon={<SendIcon />}>
-                Send
-              </Button>
             </Box>
+          </CardContent>
+          <Box sx={{ display: "flex", alignItems: "center", pl: 1, pb: 1 }}>
+            <Button variant="contained" endIcon={<SendIcon />}>
+              Send
+            </Button>
           </Box>
-        </Card>
-      </Container>
-    </>
+        </Box>
+      </Card>
+    </Container>
   );
 };
 
